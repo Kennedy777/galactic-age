607549ae6e02f844f75926c69e81517edd127651
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-20 14:55:48 -0700

    Add basic user interface logic.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-//
-// $(document).ready(function(){
-//
-// });
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $("form").submit(function() {
+    let todayDate = new Date();
+    let todayYear = todayDate.getFullYear();
+    const inputBirthYear = parseInt($("#birthyear").val());
+    let inputLifeExpectancy = parseInt($("#birthyear").val());
+    let newUser = new Age(inputBirthYear);
+
+    newUser.setAgeToday(todayYear);
+    newUser.setLifeExpectancy(inputLifeExpectancy);
+    newUser.setRemainingLife();
+
+    let output = newUser.ageEarthToMercury();
+    let output = newUser.ageEarthToVenus();
+    let output = newUser.ageEarthToMars();
+    let output = newUser.ageEarthToJupiter();
+
+    let outputMercury = newUser.remainingLifeToMercury();
+    let outputVenus = newUser.remainingLifeToVenus();
+    let outputMars = newUser.remainingLifeToMars();
+    let outputJupiter = newUser.remainingLifeToJupiter();
+
+  });
+});
