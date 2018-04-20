09e983fc3a08d5a53004e5a79a8885df1dcaa4c6
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-04-20 15:51:07 -0700

    Add logic to display galactic ages on submit.

--- src/main.js
+++ src/main.js
@@ -3,27 +3,53 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+import { Age } from './../src/age.js';
+
 $(document).ready(function() {
-  $("form").submit(function() {
+  $(".solar-system").hide();
+  $("form").submit(function(event) {
+    event.preventDefault();
+
     let todayDate = new Date();
     let todayYear = todayDate.getFullYear();
     const inputBirthYear = parseInt($("#birthyear").val());
-    let inputLifeExpectancy = parseInt($("#birthyear").val());
+    let inputLifeExpectancy = parseInt($("#life").val());
     let newUser = new Age(inputBirthYear);
 
     newUser.setAgeToday(todayYear);
     newUser.setLifeExpectancy(inputLifeExpectancy);
     newUser.setRemainingLife();
 
-    let output = newUser.ageEarthToMercury();
-    let output = newUser.ageEarthToVenus();
-    let output = newUser.ageEarthToMars();
-    let output = newUser.ageEarthToJupiter();
+    let ageMercury = newUser.ageEarthToMercury();
+    let ageVenus = newUser.ageEarthToVenus();
+    let ageMars = newUser.ageEarthToMars();
+    let ageJupiter = newUser.ageEarthToJupiter();
 
-    let outputMercury = newUser.remainingLifeToMercury();
-    let outputVenus = newUser.remainingLifeToVenus();
-    let outputMars = newUser.remainingLifeToMars();
-    let outputJupiter = newUser.remainingLifeToJupiter();
+    let remainingLifeMercury = newUser.remainingLifeToMercury();
+    let remainingLifeVenus = newUser.remainingLifeToVenus();
+    let remainingLifeMars = newUser.remainingLifeToMars();
+    let remainingLifeJupiter = newUser.remainingLifeToJupiter();
 
+    let remainingLife = newUser.getRemainingLife();
+
+    if (remainingLife > 0) {
+      $(".mercury .life").text('You have ' + remainingLifeMercury + ' years of life left');
+      $(".venus .life").text('You have ' + remainingLifeVenus + ' years of life left');
+      $(".mars .life").text('You have ' + remainingLifeMars + ' years of life left');
+      $(".jupiter .life").text('You have ' + remainingLifeJupiter + ' years of life left');
+    } else {
+      $(".mercury .life").text('Wow! You have lived '+ Math.abs(remainingLifeMercury) + ' years past your life expectancy');
+      $(".venus .life").text('Wow! You have lived '+ Math.abs(remainingLifeVenus) + ' years past your life expectancy');
+      $(".mars .life").text('Wow! You have lived '+ Math.abs(remainingLifeMars) + ' years past your life expectancy');
+      $(".jupiter .life").text('Wow! You have lived '+ Math.abs(remainingLifeJupiter) + ' years past your life expectancy');
+    }
+
+    $(".mercury .age").text(ageMercury);
+    $(".venus .age").text(ageVenus);
+    $(".mars .age").text(ageMars);
+    $(".jupiter .age").text(ageJupiter);
+
+    $(".entry-form").hide();
+    $(".solar-system").show();
   });
 });
